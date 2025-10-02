243d2fc33f7c4ed8e4b2d305ba2fb6d8932ec869
Diego Forcen <diego@Mac-mini-de-Diego.local>
2025-10-02 19:36:51 +0200

    feat:variables y conversiones basicas

---
+++ 01_fundamentos/src/tipos_basicos.py
@@ -0,0 +1,16 @@
+def demo_tipos():
+    edad: int = 43
+    altura: float = 1.85
+    nombre: str = "Diego"
+    es_manager: bool = True
+
+    print("Edad:", edad, type(edad))
+    print("Altura:", altura, type(altura))
+    print("Nombre:", nombre, type(nombre))
+    print("Es manager:", es_manager, type(es_manager))
+
+    print("Edad como string:", str(edad))
+    print("Altura redondeada a int:", int(altura))
+
+if __name__ == "__main__":
+    demo_tipos()
